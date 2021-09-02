6a8796c336e50b74895f8295ba4fa2fd1b24474b
mlkrauz <AnElusiveEntity@gmail.com>
2021-09-02 19:53:58 -0400

    Basic rendering of tilemap completed.

---
+++ game/minesweeper.js
@@ -0,0 +1,52 @@
+// Set up minesweeper config
+var config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 800,
+    parent: 'body',
+    backgroundColor: '#dfe8f5',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+}
+
+var game = new Phaser.Game(config)
+var map
+
+function preload() {
+    
+    this.load.image('defaultTiles', '../assets/sprites/defaultTiles.png')
+}
+
+function create() {
+    
+    var mapStart = [
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+    ]
+
+    map = this.make.tilemap({ data: mapStart, tileWidth: 32, tileHeight: 32})
+    var tiles = map.addTilesetImage("defaultTiles")
+    var layer = map.createLayer('layer', tiles, 44, 244)
+
+}
+
+function update(time, delta) {
+
+}
