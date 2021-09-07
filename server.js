75a3a14cc0fc5b684ea3f029ada6b720f7b6daa5
kevinchewning <klcbusiness@hotmail.com>
2021-09-07 19:01:23 -0400

    added game folder to client directory via express.static

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'game')));
 
 app.use(routes);
 
